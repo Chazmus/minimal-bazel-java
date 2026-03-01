d4ab085bd71b0f94821f518c245049d59457d7ec
Chaz Bailey <charles.s.bailey@gmail.com>
2026-03-01 21:18:21 +0000

    Remove weird eclipse stuff

--- jdtls-bazel-launcher.sh
+++ jdtls-bazel-launcher.sh
@@ -3,19 +3,19 @@
 # 1. Base paths for your built product
 PRODUCT_HOME="/home/cbailey/workspace/bazel-eclipse/releng/products/jdt-bazel-ls/target/products/jdt-bazel-ls/linux/gtk/x86_64"
 JAVA_EXEC="$PRODUCT_HOME/plugins/org.eclipse.justj.openjdk.hotspot.jre.minimal.stripped.linux.x86_64_21.0.6.v20250130-0529/jre/bin/java"
-LAUNCHER_JAR=$(ls $PRODUCT_HOME/plugins/org.eclipse.equinox.launcher_*.jar | head -n 1)
 CONFIG_DIR="$PRODUCT_HOME/configuration"
+LAUNCHER_JAR=$(ls $PRODUCT_HOME/plugins/org.eclipse.equinox.launcher_*.jar | head -n 1)
 
-# 2. Workspace data directory
-# We use a unique directory per project to avoid lock conflicts
+# 2. Workspace and Logging
 WORKSPACE_ROOT="/home/cbailey/.cache/nvim/jdtls/bazel-workspace"
 mkdir -p "$WORKSPACE_ROOT"
 
-# 3. Execute JDTLS in headless mode
+# 3. Execute JDTLS in headless mode using the product's own config
 exec "$JAVA_EXEC" \
   "-Declipse.application=org.eclipse.jdt.ls.core.id1" \
   "-Dosgi.bundles.defaultStartLevel=4" \
   "-Declipse.product=org.eclipse.jdt.ls.core.product" \
+  "-Dcom.salesforce.bazel.log.dir=$WORKSPACE_ROOT/.metadata" \
   "-Dlog.level=ALL" \
   "-Xms1G" \
   "--add-modules=ALL-SYSTEM" \
